5a6e5fa6fdd0249ecf19d9db96b071ba3fdbe5d9
Soeren Sonnenburg <Soeren.Sonnenburg@googlemail.com>
2026-05-02 09:12:29 +0200

    Bump version to 1.5.2 and refresh release notes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.librelookai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.5.1"
+        versionCode = 8
+        versionName = "1.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
