55c4f527b33b5b3dd38e96d97a118fb81e04df81
Soeren Sonnenburg <Soeren.Sonnenburg@googlemail.com>
2026-06-09 23:29:32 +0200

    release: v2.4.1 (versionCode 30)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.librelookai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 29
-        versionName = "2.4.0"
+        versionCode = 30
+        versionName = "2.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
